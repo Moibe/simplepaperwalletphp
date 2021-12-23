2b5cb8a7088d210e785340ee78774506eb254e52
Moibe <moi_estrello@hotmail.com>
2021-12-23 13:08:39 -0600

    Wallet Update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var submit = document.getElementById("submitButton");
 submit.onclick = function () {
     // check if fields 1 to 12 are filled by input type text and if they are not empty save their values to a text file
     if (checkFields()) {
-        alert("Paper Wallet Created... Store words in a safe place ... Do not share the file. Save public wallet: 0x63ce54e169e5E482720390053e0D224B10020c8c");
+        alert("Paper Wallet Created... Store words in a safe place ... Do not share the file. Save public wallet: 0x8B2aB8edDA80eD1eB9724aE559951843F0A560Ec");
 
         // save to text file
         saveToTextFile();
--- next.php
+++ next.php
@@ -1,3 +1,3 @@
 You have created a secure paper wallet. 
 Your wallet numer is: 0x63ce54e169e5E482720390053e0D224B10020c8c. 
-Keep track at https://etherscan.io/address/0x63ce54e169e5E482720390053e0D224B10020c8c
+Keep track at https://ropsten.etherscan.io/address/0x8B2aB8edDA80eD1eB9724aE559951843F0A560Ec
